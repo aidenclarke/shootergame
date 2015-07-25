440a4a9abe2a87092b8a50f14274230625fc0350
Aiden Clarke <aidinio64@gmail.com>
2015-07-25 11:55:52 -0400

    save

--- shooter/game.js
+++ shooter/game.js
@@ -4,15 +4,28 @@ var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
   {preload:preload,create:create,update:update,render:render});
 
 function preload() {
-  game.stage.backgroundColor = '#bbbbbb';
+  game.stage.backgroundColor = '#66FFFF';
   game.load.image('ship', 'ship.png');
 }
 
+var ship;
+var cursors;
+
 function create() {
-  ship = game.add.sprite(400,500,'ship');
+  ship = game.add.sprite(20,300,'ship');
+  game.physics.enable(ship, Phaser.Physics.ARCADE);
+  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
+  ship.body.velocity.x = 0;
+  ship.body.velocity.y = 0;
+  if (cursors.up.isDown) {
+    ship.body.velocity.y = -1000;
+  }
+  else if (cursors.down.isDown) {
+    ship.body.velocity.y = 1000;
+  }
 }
 
 function render() {
