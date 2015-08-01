1be395e77db0e3d40af23ac231c6cb632fb945e4
Aiden Clarke <aidinio64@gmail.com>
2015-08-01 11:51:16 -0400

    save

--- shooter/game.js
+++ shooter/game.js
@@ -3,16 +3,26 @@
 var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
   {preload:preload,create:create,update:update,render:render});
 
+var ship;
+var background;
+var cursors;
+//var enemy;
+var missle;
+
 function preload() {
   game.stage.backgroundColor = '#66FFFF';
-  game.load.image('ship', 'ship.png');
+  game.load.image('background', 'background.png');
+  game.load.image('ship', 'ship.gif');
+  game.load.image('missle','missle.gif');
+  //game.load.image('enemy', 'enemy.gif');
 }
 
-var ship;
-var cursors;
-
 function create() {
+  background = game.add.tileSprite(0,0,800,600,'background');
+  background.autoScroll(-10000,10000);
+  missle = game.add.sprite(20,300,'missle');
   ship = game.add.sprite(20,300,'ship');
+  //enemy = game.add.sprite(600,300,'enemy');
   game.physics.enable(ship, Phaser.Physics.ARCADE);
   cursors = game.input.keyboard.createCursorKeys();
 }
@@ -20,11 +30,16 @@ function create() {
 function update() {
   ship.body.velocity.x = 0;
   ship.body.velocity.y = 0;
+  missle.x = ship.x;
+  missle.y = ship.y;
   if (cursors.up.isDown) {
-    ship.body.velocity.y = -300;
+    ship.body.velocity.y = -1000;
   }
   else if (cursors.down.isDown) {
-    ship.body.velocity.y = 300;
+    ship.body.velocity.y = 1000;
+  }
+  if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+    console.log("yolo")
   }
 }
 
